df81c8b190ca942ba1e83eb3ee88b59509d8eb4d
Shikhar Rai <shikharr521@gmail.com>
2025-10-30 21:31:20 +0530

    auth centered

--- src/app/sign-up/[[...sign-up]]/page.tsx
+++ src/app/sign-up/[[...sign-up]]/page.tsx
@@ -1,1 +1,10 @@
 //sign-up page
+import { SignUp } from '@clerk/nextjs'
+
+export default function Page() {
+  return (
+      <div className='flex items-center justify-center h-screen'>
+        <SignUp />
+      </div>
+    )
+}
